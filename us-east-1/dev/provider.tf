cc2515237e4c9ebcb0908e31bd85fa51b690f670
Derek Winters <github@derekwinters.com>
2021-08-17 14:02:13 -0500

    Create provider.tf

---
+++ us-east-1/dev/provider.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_version = ">= 1.0.0"
+  
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "< 4.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.region
+  default_tags {
+    region       = var.region
+    environment  = var.environment
+    product_name = var.product_name
+    provisioner  = "terraform"
+  }
+}
